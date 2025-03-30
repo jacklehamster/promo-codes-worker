7cecd7131234433be919c1516c4845b7f2d10683
Jack Le Hamster <jacklehamster@gmail.com>
2025-03-30 14:15:08 -0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 import { listSheetsAndFetchData, updateSheetRow } from "@dobuki/google-sheet-db";
 import { createFetchFromSheet, createUpdateSheet, createNoPromoPage, findPromoForUid, redeemNextPromo, retrievePromoData, WorkerHeaders, } from "@dobuki/promo-codes";
 
-const REGEX_PROMO = /^\/promo\/([^/]+)(\/(redeem)?)?$/;
+const REGEX_PROMO = /^\/([A-Za-z0-9.-]+)(\/(redeem)?)?$/;
 
 export default {
   async fetch(request: Request, env: any): Promise<Response> {
     const url = new URL(request.url);
     if (url.pathname === '/favicon.ico') {
-      const favicon = generateFavicon();
-      return new Response(favicon, {
-        headers: {
-          'Content-Type': 'image/x-icon',
-          'Cache-Control': 'public, max-age=86400',
-        }
-      });
+      return Response.redirect("https://i0.wp.com/bignutsgames.com/wp-content/uploads/2024/10/big_nuts_logo.png");
     }
 
     const [, app, , redeem] = url.pathname.match(REGEX_PROMO) ?? [];
@@ -27,7 +21,7 @@ export default {
       const cookieStore = workerHeaders.getCookieStore();
       if (redeem) {
         if (request.method === "POST") {
-          //  POST /promo/app.id/redeem
+          //  POST /app.id/redeem
           const updatePromo = createUpdateSheet(SHEET_ID, credentials, updateSheetRow);
           const formData = await request.formData();
           const source = formData.get('src')?.toString();
@@ -56,7 +50,7 @@ export default {
             });
           }
         } else if (request.method === "GET") {
-          //  GET /promo/app.id/redeem
+          //  GET /app.id/redeem
           const promoInfo = await findPromoForUid({
             sheetId: SHEET_ID,
             app,
@@ -75,9 +69,9 @@ export default {
             });
           }
         }
-        return Response.redirect(`/promo/${app}`, 302);
+        return Response.redirect(`/${app}`, 302);
       } else {
-        //  GET /promo/app.id
+        //  GET /app.id
         const promoInfo = await retrievePromoData(SHEET_ID, {
           sheetName: app,
           app,
@@ -93,7 +87,7 @@ export default {
           });
         } else {
           const headers = makeHeaders("text/html", workerHeaders.responseCookies);
-          return new Response(promoInfo?.createPage(`/promo/${app}/redeem`) ?? createNoPromoPage({ appId: app }), {
+          return new Response(promoInfo?.createPage(`/${app}/redeem`) ?? createNoPromoPage({ appId: app }), {
             headers,
           });
         }
@@ -116,10 +110,4 @@ function makeHeaders(contentType: string, cookies: string[]) {
     headers.append("Set-Cookie", cookie);
   });
   return headers;
-}
-
-function generateFavicon() {
-  return new Uint8Array([
-    137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 16, 0, 0, 0, 16, 8, 6, 0, 0, 0, 42, 187, 137, 0, 0, 0, 4, 73, 68, 65, 84, 120, 94, 99, 96, 96, 96, 96, 96, 64, 4, 255, 242, 1, 96, 128, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130
-  ]);
 }
